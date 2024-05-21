4c9bc480a828b235701794bb7c77b23c2e2335e5
Igor <cuplcomp@yandex.ru>
2024-05-21 13:22:33 +0500

    куча дополнений

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/newjupiter/SyncWorker.java
@@ -0,0 +1,32 @@
+package com.example.newjupiter;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 8, 0}, k = 1, d1 = {"\u00000\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010 \n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0002\b\u0004\b\u0007\u0018\u00002\u00020\u0001B\u0015\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0005\u00a2\u0006\u0002\u0010\u0006J\u0011\u0010\u0007\u001a\u00020\bH\u0096@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\tJ\u0017\u0010\n\u001a\b\u0012\u0004\u0012\u00020\f0\u000bH\u0082@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\tJ\u0019\u0010\r\u001a\u00020\u000e2\u0006\u0010\u000f\u001a\u00020\fH\u0082@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\u0010J\u0011\u0010\u0011\u001a\u00020\u000eH\u0082@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\t\u0082\u0002\u0004\n\u0002\b\u0019\u00a8\u0006\u0012"}, d2 = {"Lcom/example/newjupiter/SyncWorker;", "Landroidx/work/CoroutineWorker;", "context", "Landroid/content/Context;", "params", "Landroidx/work/WorkerParameters;", "(Landroid/content/Context;Landroidx/work/WorkerParameters;)V", "doWork", "Landroidx/work/ListenableWorker$Result;", "(Lkotlin/coroutines/Continuation;)Ljava/lang/Object;", "getData", "", "error/NonExistentClass", "insert", "", "data", "(Lerror/NonExistentClass;Lkotlin/coroutines/Continuation;)Ljava/lang/Object;", "updateData", "app_debug"})
+public final class SyncWorker extends androidx.work.CoroutineWorker {
+    
+    public SyncWorker(@org.jetbrains.annotations.NotNull
+    android.content.Context context, @org.jetbrains.annotations.NotNull
+    androidx.work.WorkerParameters params) {
+        super(null, null);
+    }
+    
+    @org.jetbrains.annotations.Nullable
+    @java.lang.Override
+    public java.lang.Object doWork(@org.jetbrains.annotations.NotNull
+    kotlin.coroutines.Continuation<? super androidx.work.ListenableWorker.Result> continuation) {
+        return null;
+    }
+    
+    private final java.lang.Object getData(kotlin.coroutines.Continuation<? super java.util.List<? extends error.NonExistentClass>> continuation) {
+        return null;
+    }
+    
+    private final java.lang.Object insert(error.NonExistentClass data, kotlin.coroutines.Continuation<? super kotlin.Unit> continuation) {
+        return null;
+    }
+    
+    private final java.lang.Object updateData(kotlin.coroutines.Continuation<? super kotlin.Unit> continuation) {
+        return null;
+    }
+}
